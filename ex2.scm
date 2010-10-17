ff5ee1ce4bac844e83377c5ab780d4884aa2ebe0
Aparna <apar.sridhar@gmail.com>
2010-10-16 23:13:54 -0500

    exercise 1.2

--- ex2.scm
+++ ex2.scm
@@ -1,11 +1,11 @@
 Translate the following expression into prefix form
 
 (/ (+ 5 
-      4 
-      (- 2 
+	4 
+	(- 2 
 		(- 3 
-	    	(+ 6 
-	       		(/ 4 5))))) 
+			(+ 6 
+				(/ 4 5))))) 
    (* 3 
        (- 6 2) 
        (- 2 7)))
