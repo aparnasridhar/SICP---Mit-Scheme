1f52c79eb7177cd05f4e7f82b1b8e8dbd237f7a9
Aparna <apar.sridhar@gmail.com>
2010-10-17 12:06:09 -0500

    exercise 1.1

--- ex1.scm
+++ ex1.scm
@@ -1,47 +1,32 @@
-Below is a sequence of expressions. What is the result printed by the interpreter in response to each expression? Assume that the sequence is to be evaluated in the order in which it is presented. 
+10
 
-10 ;Value: 10 
+(+ 5 3 4)
 
-(+ 5 3 4) 
-;Value: 12 
+(- 9 1)
 
-(- 9 1) 
-;Value: 8 
+(/ 6 2)
 
-(/ 6 2) 
-;Value: 3 
+(+ (* 2 4) (- 4 6))
 
-(+ (* 2 4) (- 4 6)) 
-;Value: 6 
+(define a 3)
 
-(define a 3) 
-;Value: a 
+(define b (+ a 1))
 
-(define b (+ a 1)) 
+(+ a b (* a b))
 
-;Value: b 
+(= a b)
 
-(+ a b (* a b)) 
-;Value: 19 
+(if (and (> b a) (< b (* a b)))
+  b
+  a)
 
-(= a b) 
-;Value: #f 
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
 
-(if (and (> b a) (< b (* a b))) 
-b 
-a) 
-;Value: 4 
+(+ 2 (if (> b a) b a))
 
-(cond ((= a 4) 6) 
-	((= b 4) (+ 6 7 a)) 
-	(else 25)) 
-;Value: 16 
-
-(+ 2 (if (> b a) b a)) 
-;Value: 6 
-
-(* (cond ((> a b) a) 
-		((< a b) b) 
-		(else -1)) 
- 	(+ a 1)) 
-;Value: 16
+(* (cond ((> a b) a)
+	 ((< a b) b)
+	 (else -1))
+   (+ a 1))
